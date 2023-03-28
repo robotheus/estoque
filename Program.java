3e374c260e6808b5772a7e6808b0aa5cf597fc24
Matheus Henrique <91208015+robotheus@users.noreply.github.com>
2023-03-28 09:14:27 -0300

    Add files via upload

--- Program.java
+++ Program.java
@@ -1,28 +1,9 @@
-import java.util.Scanner;
+import visao.MenuGeral;
+import persistencia.BancoDeDados;
 
 public class Program{
-    public static void main(String[] matheus){
-        Scanner sc = new Scanner(System.in);
-
-        while(true){
-            System.out.println("--------------------stocker--------------------");
-            System.out.println("Selecione seu tipo de usuário ou 0 para SAIR:");
-            System.out.println("    Digite 1 para SETOR.");
-            System.out.println("    Digite 2 para USUARIO.");
-            System.out.println("    Digite 3 para BEM.");
-            System.out.println("    Digite 4 para TROCA.");
-
-            int op = sc.nextInt();
-            
-            switch (op) {
-                case 1:
-                    sc.close();
-                    break;
-                
-                default:
-                    sc.close();
-                    break;
-            }
-        }
+    public static void main(String[] args){
+        BancoDeDados banco = new BancoDeDados();
+        MenuGeral.Menu(banco);
     }
 }
