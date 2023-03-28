df51ab7590e3d608a815ede6e31ab85f70d15433
Matheus Henrique <91208015+robotheus@users.noreply.github.com>
2023-03-27 21:07:56 -0300

    Add files via upload

--- Program.java
+++ Program.java
@@ -1,24 +1,21 @@
 import java.util.Scanner;
-import modelo.Administrador;
 
 public class Program{
     public static void main(String[] matheus){
         Scanner sc = new Scanner(System.in);
-        Administrador adm = new Administrador();
 
         while(true){
             System.out.println("--------------------stocker--------------------");
             System.out.println("Selecione seu tipo de usuário ou 0 para SAIR:");
-            System.out.println("    Digite 1 para Administrador do sistema.");
-            System.out.println("    Digite 2 para Chefia.");
-            System.out.println("    Digite 3 para Funcionário.");
+            System.out.println("    Digite 1 para SETOR.");
+            System.out.println("    Digite 2 para USUARIO.");
+            System.out.println("    Digite 3 para BEM.");
+            System.out.println("    Digite 4 para TROCA.");
+
             int op = sc.nextInt();
             
             switch (op) {
                 case 1:
-                    System.out.println("Digite a senha de ADMINISTRADOR: ");
-                    String pass = sc.next();
-                    if (pass.equals(adm.getName())) adm.getVisao().menu();
                     sc.close();
                     break;
                 
