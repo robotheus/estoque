db056443519e5e6bcb6bf4364aaca9ee52e52b01
Matheus Henrique <91208015+robotheus@users.noreply.github.com>
2023-03-29 09:43:56 -0300

    Add files via upload

--- visao/MenuGeral.java
+++ visao/MenuGeral.java
@@ -7,26 +7,30 @@ import persistencia.BancoDeDados;
 public class MenuGeral {
     public static void Menu(BancoDeDados banco){
         Scanner sc = new Scanner(System.in);
+    
+        System.out.println("--------------------stocker--------------------");
+        System.out.println("Selecione seu tipo de usuário ou 0 para SAIR:");
+        System.out.println("    Digite 1 para SETOR.");
+        System.out.println("    Digite 2 para USUARIO.");
+        System.out.println("    Digite 3 para BEM.");
+        System.out.println("    Digite 4 para TROCA.");
 
-        while(true){
-            System.out.println("--------------------stocker--------------------");
-            System.out.println("Selecione seu tipo de usuário ou 0 para SAIR:");
-            System.out.println("    Digite 1 para SETOR.");
-            System.out.println("    Digite 2 para USUARIO.");
-            System.out.println("    Digite 3 para BEM.");
-            System.out.println("    Digite 4 para TROCA.");
-
-            int op = sc.nextInt();
-            
-            switch (op) {
-                case 1:
-                    VisaoSetor.MenuSetor(banco);
-                    break;
+        int op = sc.nextInt();
+        
+        switch (op) {
+            case 1:
+                VisaoSetor.MenuSetor(banco);
+                break;
                 
-                default:
-                    
-                    break;
-            }
+            case 2:
+                VisaoUsuario.MenuUsuario(banco);
+                break;
+            
+            default:
+                break;
         }
+
+        sc.close();
+    
     }  
 }
