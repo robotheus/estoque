d577d2148dcd55d139927411bffca478dd8a1540
Matheus Henrique <91208015+robotheus@users.noreply.github.com>
2023-03-29 16:52:00 -0300

    Add files via upload

--- visao/VisaoTroca.java
+++ visao/VisaoTroca.java
@@ -13,10 +13,35 @@ public class VisaoTroca {
             System.out.println("--------------------TROCA--------------------");
             System.out.println("    Digite 0 para RETORNAR ao menu inicial.");
             System.out.println("    Digite 1 para REALIZAR um TROCA.");
-            System.out.println("    Digite 2 para REMOVER um USUARIO.");
-            System.out.println("    Digite 3 para ALTERAR o USUARIO (Trocar de setor).");
-            System.out.println("    Digite 4 para VISUALIZAR VIA ID.");
-            System.out.println("    Digite 5 para VISUALIZAR TODOS.");
+            System.out.println("    Digite 2 para ALTERAR uma TROCA (REMOVER OU ALTERAR ITENS).");
+            System.out.println("    Digite 3 para VISUALIZAR VIA ID.");
+            System.out.println("    Digite 4 para VISUALIZAR TODOS.");
+
+            op = sc.nextInt();
+
+            switch(op) {
+                case 0: 
+                    return;
+                
+                case 1:
+                    
+                    break;
+                        
+                case 2:
+                    
+                    break;
+                        
+                case 3:
+                    
+                    break;
+                    
+                case 4:
+                    
+                    break;    
+                    
+                default:
+                    break;
+            }    
         }
     }
 }
