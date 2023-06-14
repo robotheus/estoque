2bfe9c3e6e289e5227590c7411168210d571729e
Matheus Henrique <91208015+robotheus@users.noreply.github.com>
2023-06-14 19:34:30 -0300

    Add files via upload

--- Program.java
+++ Program.java
@@ -1,9 +1,9 @@
-import visao.MenuGeralGUI;
+import visao.MenuGeral;
 import persistencia.*;
 
 public class Program{
     public static void main(String[] args){
         BancoDeDados banco = new BancoDeDados();
-        new MenuGeralGUI(banco);
+        new MenuGeral(banco);
     }
 }
