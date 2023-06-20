47878cf5bc80f63e7108341ed386e7f557a96259
Matheus Henrique <91208015+robotheus@users.noreply.github.com>
2023-06-20 17:52:55 -0300

    Update Usuario.java

--- modelo/Usuario.java
+++ modelo/Usuario.java
@@ -1,18 +1,18 @@
 package modelo;
 
 public class Usuario extends Entidade{
-    private Entidade setor;
+    private Setor setor;
 
     public Usuario(String name){
         super(name);
         this.setor = null;
     }
 
-    public Entidade getSetor() {
+    public Setor getSetor() {
         return setor;
     }
 
-    public void setSetor(Entidade setor) {
+    public void setSetor(Setor setor) {
         this.setor = setor;
     }
 
